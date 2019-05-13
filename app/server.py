7735705526341282619b96bad77fc8380838a82c
Polegar22 <denis.casselle@gmail.com>
2019-05-13 18:47:13 +0200

    wrong commit

--- app/server.py
+++ app/server.py
@@ -102,4 +102,4 @@ async def generate_text(request):
 
 if __name__ == '__main__':
     if 'serve' in sys.argv:
-        uvicorn.run(app, host='0.0.0.0', port=8000)
+        uvicorn.run(app, host='0.0.0.0', port=8080)
